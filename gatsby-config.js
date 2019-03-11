3eaae0df7f2939cb7a8788ed68757e2039767e14
Kanniappan <bhuvana.m.kanniappan@accenture.com>
2019-03-11 18:48:26 +0530

    Include Strapi CMS Data Provider

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: 'My super blog',
+    description: 'Gatsby blog with Strapi',
+    author: 'Strapi team'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
+          'article',
+          'user'
+        ],
+        queryLimit: 1000,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -18,7 +38,47 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: 'gatsby-sample/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+  ],
+}
+
+/*
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Starter - Dimension V2",
+    author: "Hunter Chang",
+    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+  },
+  pathPrefix: `/gatsby-sample`,
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: 'gatsby-sample/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
 }
+*/
